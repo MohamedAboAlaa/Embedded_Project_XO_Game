8d32df0c68ddadbe4ba34cbde02c679a80d7c004
abdelrhamanosama <121834592+abdelrhamanosama@users.noreply.github.com>
2024-12-22 21:38:24 +0200

    Update DAC.h

--- XO_Game/Drivers_Header/DAC.h
+++ XO_Game/Drivers_Header/DAC.h
@@ -1,9 +1,12 @@
-// DAC.h
-// Runs on LM4F120 or TM4C123, 
-// edX lab 13 
-// Header file for the 4-bit digital to analog converter
-// Daniel Valvano, Jonathan Valvano
-// March 13, 2014
+/******************************************************************************
+ * Module: UART
+ *
+ * File Name: UART.h
+ *
+ * Description: Header file for UART functionality.
+ *
+ * Authors: El-Osood El-Mefrhda
+ ******************************************************************************/
 
 // **************DAC_Init*********************
 // Initialize 4-bit DAC 
